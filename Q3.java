54e54ae5c931907d59e2de70728faa701ca441a6
huimin7 <147672948+huimin7@users.noreply.github.com>
2023-11-08 18:34:58 +0800

    Q3.java

--- Q3.java
+++ Q3.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Chuimin
- */
 import java.util.Scanner;
 public class Q3 {
     public static void main(String[]args){
